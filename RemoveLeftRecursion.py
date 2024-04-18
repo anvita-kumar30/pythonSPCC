c9feb01720429d7107a1ee2bc524bc4588ebd204
Anvita <kumaranvita30@gmail.com>
2024-04-18 21:15:28 +0530

    init

---
+++ RemoveLeftRecursion.py
@@ -0,0 +1,39 @@
+def eliminate_left_recursion(grammar):
+    non_terminals = list(grammar.keys())
+    for A in non_terminals:
+        productions = grammar[A]
+        alpha = []
+        beta = []
+
+        # Classify productions into alpha and beta
+        for production in productions:
+            if production[0] == A:
+                alpha.append(production[1:])
+            else:
+                beta.append(production)
+
+        if alpha:
+            # Create a new non-terminal A' for indirect left recursion
+            A_prime = A + "'"
+            grammar[A] = [production + (A_prime,) for production in beta]
+            # Update grammar with new productions for A'
+            grammar[A_prime] = [production + (A_prime,) for production in alpha] + [('ε',)]
+    return grammar
+
+def print_grammar(grammar):
+    for non_terminal, productions in grammar.items():
+        print(f"{non_terminal} -> {' | '.join([''.join(prod) for prod in productions])}")
+
+# Test the grammar elimination function
+grammar = {
+    'A': [('A', 'a'), ('A', 'b'), ('c',), ('d', 'A')],
+    'B': [('B', 'b'), ('B', 'c'), ('d',)]
+}
+
+print("Original Grammar:")
+print_grammar(grammar)
+
+eliminate_left_recursion(grammar)
+
+print("\nGrammar without Left Recursion:")
+print_grammar(grammar)
